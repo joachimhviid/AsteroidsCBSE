0ffc800ca8930733c6bd3cc9d8aec398d27cb48a
joachimhviid <joac1099@gmail.com>
2024-05-20 12:25:25 +0200

    Define pre and post condition on interfaces

--- common/src/main/java/services/IGamePluginService.java
+++ common/src/main/java/services/IGamePluginService.java
@@ -2,6 +2,11 @@ package services;
 
 import com.almasb.fxgl.entity.GameWorld;
 
+/**
+ * Interface for game plugin services
+ * Precondition: The game world must be initialized
+ * Postcondition: The game plugin service is started or stopped
+ */
 public interface IGamePluginService {
     void start(GameWorld world);
     void stop(GameWorld world);
--- common/src/main/java/services/IInputService.java
+++ common/src/main/java/services/IInputService.java
@@ -1,5 +1,10 @@
 package services;
 
+/**
+ * Interface for input services
+ * Precondition: The game must be loaded
+ * Postcondition: The input is set
+ */
 public interface IInputService {
     void setInput();
 }
